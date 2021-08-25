e4595f794cd5fadacfd0e0116fd1f4bf1af875ec
swagoverlord <azcooldude8@gmail.com>
2021-08-25 22:30:35 +1000

    Styling

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,10 +1,26 @@
 package application;
 
-
 import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Controller {
-	public void Quit(ActionEvent e) {
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent parent;
+	
+	public void MoveToRegister(ActionEvent event) {
+		System.out.println("Going to Register page.");
+	}
+	
+	public void MoveToLogin(ActionEvent event) {
+		System.out.println("Going to Login page.");
+	}
+	
+	public void Quit(ActionEvent event) {
+		System.out.println("Quiting Application.");
 		System.exit(0);
 	}
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -15,9 +15,10 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			String css = getClass().getResource("application.css").toExternalForm();
+			scene.getStylesheets().add(css);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
