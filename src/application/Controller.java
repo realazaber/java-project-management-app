87d10d5d5ffbeac7d097a7942b4aa2bcb4e7a4f5
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-08-26 01:59:42 +1000

    Fixing register page

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -9,35 +11,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.*;
 
+import java.sql.*;
+
 public class Controller {
 	
 	private Stage stage;
 	private Scene scene;
 	
-	public void MoveToRegister(ActionEvent event) throws IOException {
-		System.out.println("Going to Register page.");
-		Parent root = FXMLLoader.load(getClass().getResource("Register.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root,800,600);
-		stage.setScene(scene);
-		String css = getClass().getResource("application.css").toExternalForm();
-		scene.getStylesheets().add(css);
-		stage.show();
-	}
+	
 	
 
-	
-	public void MoveToLogin(ActionEvent event) throws IOException {
-		System.out.println("Going to Login page.");
-		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root,800,600);
-		stage.setScene(scene);
-		String css = getClass().getResource("application.css").toExternalForm();
-		scene.getStylesheets().add(css);
-		stage.show();
-	}
-	
 	public void MoveToHome(ActionEvent event) throws IOException {
 		System.out.println("Going to Home page.");
 		Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
