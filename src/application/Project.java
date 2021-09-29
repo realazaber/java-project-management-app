a61b2e396cfffd77064822eac5cf7f8921e13e11
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-09-29 23:21:47 +1000

    getters and setters for project

--- src/application/Project.java
+++ src/application/Project.java
@@ -5,5 +5,34 @@ public class Project {
 	int userID;
 	String projectName;
 	
+	public Project() {
+		
+	}
+	
+	public int getProjectID() {
+		return projectID;
+	}
+	
+	public void setProjectID(int projectID) {
+		this.projectID = projectID;
+	}
+	
+	public int getUserID() {
+		return userID;
+	}
+	
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	
+	public String getProjectName() {
+		return projectName;
+	}
+	
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	
+	
 	
 }
