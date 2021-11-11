5abdc27f50c1ecf4772b1b86f02dea3ac5cd8db9
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-11-11 12:41:57 +1100

    finished

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Heading from './components/Heading';
 import Nav from './components/Nav';
 import FAQ from './components/FAQ';
-import Gallery from './components/Gallery';
+import Gallery from './components/Gallery/Gallery';
 import Download from './components/Download';
 import Contact from './components/Contact';
 
